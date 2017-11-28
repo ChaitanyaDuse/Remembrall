0344f677aec4a0378d55cb4a151ebf0c1b0def89
chaitanyaduse <chaitanyaduse31@gmail.com>
2017-11-28 20:04:07 +0530

    unused class for proguard test added

---
+++ app/src/main/java/com/dnr/todoapksize/data/UnusedClassForProguard.java
@@ -0,0 +1,26 @@
+package com.dnr.todoapksize.data;
+
+/**
+ * Created by cduse on 11/28/2017.
+ */
+
+public class UnusedClassForProguard {
+    private int mThisIsMyId;
+    private int mThisIsMyTitle;
+
+    public int getmThisIsMyId() {
+        return mThisIsMyId;
+    }
+
+    public void setmThisIsMyId(int mThisIsMyId) {
+        this.mThisIsMyId = mThisIsMyId;
+    }
+
+    public int getmThisIsMyTitle() {
+        return mThisIsMyTitle;
+    }
+
+    public void setmThisIsMyTitle(int mThisIsMyTitle) {
+        this.mThisIsMyTitle = mThisIsMyTitle;
+    }
+}
